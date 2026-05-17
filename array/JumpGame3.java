15908666e616db34d195b8dca9fb7a4e19265ec5
Prachi Dubey <006prachi@gmail.com>
2026-05-17 15:24:52 +0530

    DFS

---
+++ array/JumpGame3.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class JumpGame3 {
+
+    public static boolean canReach(int[] arr, int start) {
+
+        boolean[] visited = new boolean[arr.length];
+        return dfs(arr, start, visited);
+
+    }
+
+
+    public static boolean dfs(int[] arr, int index, boolean[] visited) {
+
+        if(index < 0 || index >= arr.length) {
+            return false;
+        }
+
+        if(visited[index]) {
+            return false;
+        }
+
+        if(arr[index] == 0) {
+            return true;
+        }
+
+        visited[index] = true;
+
+        boolean forward = dfs(arr, index + arr[index], visited);
+        boolean backward = dfs(arr, index - arr[index], visited);
+
+        return forward || backward;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 2, 3, 0, 3, 1, 2};
+        int start = 5;
+
+        boolean result = canReach(arr, start);
+        System.out.println(result);
+    }
+    
+}
