3cf08a7728820c9e8766ba94893b8d0c7187a782
Prachi Dubey <006prachi@gmail.com>
2026-06-07 13:20:07 +0530

    Monotonic Array/Stack

---
+++ stack/DailyTemparature.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class DailyTemparature {
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] ans = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() &&
+                   temperatures[stack.peek()] <= temperatures[i]) {
+                stack.pop();
+            }
+
+            if (!stack.isEmpty()) {
+                ans[i] = stack.peek() - i;
+            }
+
+            stack.push(i);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+
+        int[] result = dailyTemperatures(temperatures);
+
+        System.out.println(Arrays.toString(result));
+    }
+    
+}
