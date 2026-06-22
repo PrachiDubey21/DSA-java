7f76615714430114ef74b7f8a6bc333016625673
Prachi Dubey <006prachi@gmail.com>
2026-06-22 22:07:04 +0530

    String

---
+++ String/CountBallon.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class CountBallon {
+
+    public static int maxNumberOfBalloons(String text) {
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = text.charAt(i);
+            freq[ch - 'a']++;
+
+        }
+
+        int b = freq['b' - 'a'];
+        int a = freq['a' - 'a'];
+        int l = freq['l' - 'a'] / 2;
+        int o = freq['o' - 'a'] / 2;
+        int n = freq['n' - 'a'];
+
+        return Math.min(
+                Math.min(b, a),
+                Math.min(l, Math.min(o, n))
+        );
+    }
+
+    public static void main(String[] args) {
+
+        String text = "loonbalxballpoon";
+
+        int answer = maxNumberOfBalloons(text);
+        System.out.println(answer);
+
+    }
+    
+}
