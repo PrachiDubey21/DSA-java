a53edeea50ef7ff546996b11b3fbe1e1dbfb932b
Prachi Dubey <006prachi@gmail.com>
2026-06-29 13:58:10 +0530

    DP

--- DP/MinCostToClimbStairs.java
+++ DP/MinCostToClimbStairs.java
@@ -20,6 +20,29 @@ public class MinCostToClimbStairs {
 // So answer is
 // Math.min(dp[n-1], dp[n-2]);
 
+    public static int minCostClimbingStairs(int[] cost) {
 
+        int n = cost.length;
+        int[] dp = new int[n];
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        }
+
+        return Math.min(dp[n - 1], dp[n - 2]);
+    }
+
+    public static void main(String[] args) {
+
+        int[] cost = {10, 15, 20};
+
+        int answer = minCostClimbingStairs(cost);
+        System.out.println("Minimum Cost = " + answer);
+  
+    }
 
 }
