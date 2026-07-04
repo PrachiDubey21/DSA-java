d4f01e43c9e1dbff4ccab4dcb4c86c05c29f3925
Prachi Dubey <006prachi@gmail.com>
2026-07-04 15:37:27 +0530

    DP

---
+++ DP/PascalsTriangle.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class PascalsTriangle {
+
+    public static List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+
+                if (j == 0 || j == i) {
+                    row.add(1);
+                }
+                else {
+                    int value = triangle.get(i - 1).get(j - 1)
+                              + triangle.get(i - 1).get(j);
+
+                    row.add(value);
+                }
+            }
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+
+        List<List<Integer>> ans = generate(5);
+        System.out.println(ans);
+
+    }
+    
+}
