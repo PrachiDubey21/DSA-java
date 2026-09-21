b739b09013d3a173bae508f99796c5b87f2be71b
Prachi Dubey <006prachi@gmail.com>
2026-09-21 21:57:39 +0530

    DP

---
+++ DP/MinPathSum.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class MinPathSum {
+
+    public static int Solve(int[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                if (i == 0) {
+                    grid[i][j] += grid[i][j - 1];
+                }
+                else if (j == 0) {
+                    grid[i][j] += grid[i - 1][j];
+                }
+                else {
+                    grid[i][j] += Math.min(grid[i - 1][j],grid[i][j - 1]);
+                }
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+
+        int[][] grid = {
+            {1, 3, 1},
+            {1, 5, 1},
+            {4, 2, 1}
+        };
+
+        int result = Solve(grid);
+        System.out.println(result);
+
+    }
+}
