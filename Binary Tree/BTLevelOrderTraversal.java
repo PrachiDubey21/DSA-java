c178d18f72d199e9b391c7acd88604674961d54c
Prachi Dubey <006prachi@gmail.com>
2026-09-22 20:42:38 +0530

    BFS

---
+++ Binary Tree/BTLevelOrderTraversal.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class BTLevelOrderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            ans.add(level);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(levelOrder(root));
+        
+    }
+    
+}
